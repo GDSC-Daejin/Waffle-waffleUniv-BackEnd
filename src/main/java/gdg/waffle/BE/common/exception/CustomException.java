209378b6b6c65cbbd40d86cae75b0a4fe935186b
Bemay <ilginam24@gmail.com>
2024-12-01 22:14:06 +0900

    로그인 패키지 생성 및 디렉토리 구성 완료
    # Conflicts:
    #	build.gradle
    #	gradlew
    #	src/main/resources/application.properties

---
+++ src/main/java/gdg/waffle/BE/common/exception/CustomException.java
@@ -0,0 +1,4 @@
+package gdg.waffle.BE.common.exception;
+
+public class CustomException {
+}
---
+++ src/main/java/gdg/waffle/BE/login/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package gdg.waffle.BE.login.repository;
+
+import gdg.waffle.BE.login.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
